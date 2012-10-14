e7fd12a101aff394bc52b3d2391c4ca4fb07170c
Ernest Gu <erniewarrensuper@gmail.com>
2012-10-13 22:04:39 -0700

    Skeleton for buttons `enum`
    Also commented out the `high-level functions.h` stuff since it's false.

--- RobotC/Team A/enums.h
+++ RobotC/Team A/enums.h
@@ -7,4 +7,20 @@ typedef enum Direction
 	DIRECTION_R = 1,
 };
 
+typedef enum JoystickButton
+{
+	BUTTON_A,
+	BUTTON_B,
+	BUTTON_X,
+	BUTTON_Y,
+	BUTTON_LB,
+	BUTTON_RB,
+	BUTTON_LT,
+	BUTTON_RT,
+	BUTTON_START,
+	BUTTON_SELECT,
+	BUTTON_MENU,
+	BUTTON_LOGO,	//the Logitech logo button-thingy
+};
+
 #endif
