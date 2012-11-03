b8075cd656d99d3b526ec3e12a421e203d65a1ad
Ernest Gu <erniewarrensuper@gmail.com>
2012-11-03 09:10:32 -0700

    Added a few global variables & subroutines
    Also added some commenting.

--- RobotC/6424/Header Files/global vars.h
+++ RobotC/6424/Header Files/global vars.h
@@ -1,6 +1,18 @@
 #ifndef GLOBAL_VARS_H
 #define GLOBAL_VARS_H
 
+
+// The full speed these motors should be PID-regulated at.
+int g_FullPowerL = 100;
+int g_FullPowerR = 100;
+int g_FullPowerLift = 100;
+
+// These angles are added onto the current encoder count of the
+// lift motor (the "current" value varies at startup).
+int g_TopLiftAngle = 0;		//TODO
+int g_MiddleLiftAngle = 0;	//TODO
+int g_BottomLiftAngle = 0;	//TODO
+
 // Mask for the "bitmap" values from the controller for everything
 // we don't need (other than A/B/X/Y/JOYL/JOYR):
 	//   0000 1100 0000 1111
@@ -9,6 +21,7 @@
 	//   0000 ??00 0000 ????
 // This information was gleaned from the definition of joy1Btn().
 	// 2^1 +2^2 +2^3 +2^4 +2^11 +2^12 = 2+4+8+16+2048+4096 = 6174
-short CONTROLLER_MASK = 6174;
+short g_ControllerMask= 6174;
+
 
 #endif
