89343de4d3494097fa882146583036c10b63dfd4
Ernest Gu <erniewarrensuper@gmail.com>
2012-11-11 17:24:58 -0800

    Fixed multitasking bug (hopefully)
    Now working but not switching between tasks. Wonder what priority `task
    main()` is.

--- RobotC/6424/Header Files/subroutines.h
+++ RobotC/6424/Header Files/subroutines.h
@@ -5,55 +5,52 @@
 task sub_PutRingOn()
 {
 	//do stuff
-	//EndTimeSlice();
+	EndTimeSlice();
 }
 
 task sub_TakeRingOff()
 {
 	//do stuff
-	//EndTimeSlice();
+	EndTimeSlice();
 }
 
 task sub_LiftToTop()
 {
-	// `-10` for a bit more precision. Might even break this up into
-	// a fast section and a slower section (to fine-tune position).
+	// Might add a fine-tune section to this.
 	Motor_ExactRotation(motor_lift, g_TopLiftAngle, g_FullMotorPower);
 	while( Motor_GetEncoder(motor_lift) != g_TopLiftAngle)
 	{
 		Time_Wait(50);
 	}
-	//EndTimeSlice();
+	EndTimeSlice();
 }
 
 task sub_LiftToMiddle()
 {
-	// `-10` for a bit more precision. Might even break this up into
-	// a fast section and a slower section (to fine-tune position).
+	// Might add a fine-tune section to this.
 	Motor_ExactRotation(motor_lift, g_MiddleLiftAngle, g_FullMotorPower);
 	while( Motor_GetEncoder(motor_lift) != g_MiddleLiftAngle)
 	{
 		Time_Wait(50);
 	}
-	//EndTimeSlice();
+	EndTimeSlice();
 }
 
 task sub_LiftToBottom()
 {
-	// `-10` for a bit more precision. Might even break this up into
-	// a fast section and a slower section (to fine-tune position).
+	// Might add a fine-tune section to this.
 	Motor_ExactRotation(motor_lift, g_BottomLiftAngle, g_FullMotorPower);
 	while( Motor_GetEncoder(motor_lift) != g_BottomLiftAngle)
 	{
 		Time_Wait(50);
 	}
-	//EndTimeSlice();
+	EndTimeSlice();
 }
 
 task sub_WeighRings()
 {
-	//I have no idea how to implement this yet.
-	//EndTimeSlice();
+	// We might not implement this at all. Heh.
+	EndTimeSlice();
 }
 
 task sub_DeployRamp()
