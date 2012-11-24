c38a4ee4eb474c9222d59de658d73808d5ca30d1
Ernest Gu <erniewarrensuper@gmail.com>
2012-11-24 11:15:29 -0800

    Fixed `waitForStart();` positioning
    Kieran only fixed the neccessary ones, not all of them.

--- RobotC/6424/Autonomous Programs/A_Position-R.c
+++ RobotC/6424/Autonomous Programs/A_Position-R.c
@@ -1,6 +1,5 @@
 #pragma config(Hubs,  S1, HTServo,  HTMotor,  HTMotor,  none)
-#pragma config(Sensor, S1,     ,               sensorI2CMuxController)
-#pragma config(Sensor, S2,     infrared,       sensorHiTechnicIRSeeker1200)
+#pragma config(Sensor, S2,     infrared,       sensorI2CCustom)
 #pragma config(Sensor, S3,     weight,         sensorHiTechnicTouchMux)
 #pragma config(Sensor, S4,     touch,          sensorTouch)
 #pragma config(Motor,  motorA,          motor_popcorn, tmotorNXT, openLoop, reversed)
@@ -31,24 +30,28 @@
 
 void initializeRobot()
 {
-	// Place code here to init servos to starting positions.
 	// Sensors are config'ed and setup by RobotC (need to stabalize).
-	// Also add any settings that need to be set (other than global
-	// variables), such as max PID speed, servo update rate, etc.
 
-	Servo_SetSpeed(servo_IR, 10);	// maximum speed!
-	Servo_SetSpeed(servo_claw, 10);	// maximum speed!
+	Servo_SetSpeed(servo_IR, 10);		// maximum speed!
+	Servo_SetSpeed(servo_claw, 10);		// maximum speed!
+	Servo_SetSpeed(servo_ramp, 100);	// slowly update so ramp doesn't release.
 
-	Servo_Rotate(servo_IR, g_IRServoExtended);		// fold back up after start of tele-op
-	Servo_Rotate(servo_claw, g_clawServoExtended);	// keep it straight out after tele-op
+	Servo_Rotate(servo_IR, g_IRServoExtended);		// will fold back up in tele-op
+	Servo_Rotate(servo_claw, g_clawServoExtended);	// will be folded in tele-op
+	Servo_Rotate(servo_ramp, g_rampServoDefault);	// stops ramp from deploying
 
+	Motor_SetMaxSpeed(g_FullRegulatedPower);
 
-	Motor_SetMaxSpeed(g_FullMotorPower);
-
+	Motor_ResetEncoder(motor_L);
+	Motor_ResetEncoder(motor_R);
 	Motor_ResetEncoder(motor_lift);
 
-	nMotorEncoder[motor_lift] = 0;
-
+	// Wait this long so the claw & IR servos get to update.
+	// The ramp-release servo shouldn't move; the long update time
+	// is to prevent sudden jerks that might release the ramp.
+	// We don't need to wait for the IR sensor to stabalize since
+	// the robot doesn't read from it until it's at the first column,
+	// which should be ample time for RobotC to setup the sensor.
 	Time_Wait(10);
 
 	return;
@@ -58,9 +61,11 @@ void initializeRobot()
 
 task main()
 {
+	waitForStart();
+
 	initializeRobot();
 
-	waitForStart();
+
 
 	// The amount of time the robot...
 
