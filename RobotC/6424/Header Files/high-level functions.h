3096c8ba6318a46a0bc7affbdeda0b6a84c8cd38
Ernest Gu <erniewarrensuper@gmail.com>
2012-11-28 08:31:12 -0800

    Reversed lift motor subroutine power level
    Now that the lift motor is reversed.

--- RobotC/6424/Header Files/high-level functions.h
+++ RobotC/6424/Header Files/high-level functions.h
@@ -42,14 +42,14 @@ void Turn_Left(int time, int steerL, int steerR)
 
 void Lift_Up(int time, int power)
 {
-	Motor_Forward(motor_lift, (-1) * power);
+	Motor_Forward(motor_lift, power);
 	Time_Wait(time);
 	Motor_Stop(motor_lift);
 }
 
 void Lift_Down(int time, int power)
 {
-	Motor_Forward(motor_lift, power);
+	Motor_Forward(motor_lift, (-1) * power);
 	Time_Wait(time);
 	Motor_Stop(motor_lift);
 }
