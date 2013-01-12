e5055ac3298e61406c6271fddc215f4a507b986e
Ernest Gu <erniewarrensuper@gmail.com>
2013-01-11 22:18:55 -0800

    Converted to built-in PID control.

--- RobotC/6424/Header Files/high-level functions.h
+++ RobotC/6424/Header Files/high-level functions.h
@@ -3,145 +3,81 @@
 
 
 
-void Move_Forward(int time, int power=g_FullDrivePower)
+void Move_Forward(int angle, int power=g_FullDrivePower)
 {
-	if ( (time>=g_RampDownLength) && (power>=10) )
-	{
-		Motor_Forward(motor_L, power);
-		Motor_Forward(motor_R, power);
-		Time_Wait(time - g_RampDownLength);
-		Motor_Forward(motor_L, 10);
-		Motor_Forward(motor_R, 10);
-		Time_Wait(2);
-		Motor_Forward(motor_L, 5);
-		Motor_Forward(motor_R, 5);
-		Time_Wait(4);
-		Motor_Forward(motor_L, 3);
-		Motor_Forward(motor_R, 3);
-		Time_Wait(8);
-		Motor_Forward(motor_L, 2);
-		Motor_Forward(motor_R, 2);
-		Time_Wait(4);
-		Motor_Forward(motor_L, 1);
-		Motor_Forward(motor_R, 1);
-		Time_Wait(2);
-		Motor_Stop(motor_L);
-		Motor_Stop(motor_R);
-	}
-	else
-	{
-		Motor_Forward(motor_L, power);
-		Motor_Forward(motor_R, power);
-		Time_Wait(time);
-		Motor_Stop(motor_L);
-		Motor_Stop(motor_R);
-	}
+	int currentAngleL = Motor_GetEncoder(motor_L);
+	int currentAngleR = Motor_GetEncoder(motor_R);
+	int targetAngleL = currentAngleL+angle;
+	int targetAngleR = currentAngleR+angle;
+
+	Motor_Target(motor_L, targetAngleL);
+	Motor_Target(motor_R, targetAngleR);
+	Motor_Forward(motor_L, power);
+	Motor_Forward(motor_R, power);
+
+	while ( (abs(Motor_GetEncoder(motor_L)-targetAngleL)>g_MoveTolerance) ||
+			(abs(Motor_GetEncoder(motor_R)-targetAngleR)>g_MoveTolerance) )
+				{;}
+	Motor_Stop(motor_L);
+	Motor_Stop(motor_R);
 }
 
 
-void Move_Backward(int time, int power=g_FullDrivePower)
+void Move_Backward(int angle, int power=g_FullDrivePower)
 {
-	if ( (time>=g_RampDownLength) && (power>=10) )
-	{
-		Motor_Reverse(motor_L, power);
-		Motor_Reverse(motor_R, power);
-		Time_Wait(time - g_RampDownLength);
-		Motor_Reverse(motor_L, 10);
-		Motor_Reverse(motor_R, 10);
-		Time_Wait(2);
-		Motor_Reverse(motor_L, 5);
-		Motor_Reverse(motor_R, 5);
-		Time_Wait(4);
-		Motor_Reverse(motor_L, 3);
-		Motor_Reverse(motor_R, 3);
-		Time_Wait(8);
-		Motor_Reverse(motor_L, 2);
-		Motor_Reverse(motor_R, 2);
-		Time_Wait(4);
-		Motor_Reverse(motor_L, 1);
-		Motor_Reverse(motor_R, 1);
-		Time_Wait(2);
-		Motor_Stop(motor_L);
-		Motor_Stop(motor_R);
-	}
-	else
-	{
-		Motor_Reverse(motor_L, power);
-		Motor_Reverse(motor_R, power);
-		Time_Wait(time);
-		Motor_Stop(motor_L);
-		Motor_Stop(motor_R);
-	}
+	int currentAngleL = Motor_GetEncoder(motor_L);
+	int currentAngleR = Motor_GetEncoder(motor_R);
+	int targetAngleL = currentAngleL-angle;
+	int targetAngleR = currentAngleR-angle;
+
+	Motor_Target(motor_L, targetAngleL);
+	Motor_Target(motor_R, targetAngleR);
+	Motor_Reverse(motor_L, power);
+	Motor_Reverse(motor_R, power);
+
+	while ( (abs(Motor_GetEncoder(motor_L)-targetAngleL)>g_MoveTolerance) ||
+			(abs(Motor_GetEncoder(motor_R)-targetAngleR)>g_MoveTolerance) )
+				{;}
+	Motor_Stop(motor_L);
+	Motor_Stop(motor_R);
 }
 
-void Turn_Right(int time, int steerL, int steerR)
+void Turn_Right(int angle, int steerL, int steerR)
 {
-	//if ( (time>=g_RampDownLength) && (steerL>=10) )
-	//{
-	//	Motor_Forward(motor_L, steerL);
-	//	Motor_Reverse(motor_R, steerR);
-	//	Time_Wait(time - g_RampDownLength);
-	//	Motor_Forward(motor_L, 10);
-	//	Motor_Reverse(motor_R, 10);
-	//	Time_Wait(2);
-	//	Motor_Forward(motor_L, 5);
-	//	Motor_Reverse(motor_R, 5);
-	//	Time_Wait(4);
-	//	Motor_Forward(motor_L, 3);
-	//	Motor_Reverse(motor_R, 3);
-	//	Time_Wait(8);
-	//	Motor_Forward(motor_L, 2);
-	//	Motor_Reverse(motor_R, 2);
-	//	Time_Wait(4);
-	//	Motor_Forward(motor_L, 1);
-	//	Motor_Reverse(motor_R, 1);
-	//	Time_Wait(2);
-	//	Motor_Stop(motor_L);
-	//	Motor_Stop(motor_R);
-	//}
-	//else
-	//{
-		Motor_Forward(motor_L, steerL);
-		Motor_Reverse(motor_R, steerR);
-		Time_Wait(time);
-		Motor_Stop(motor_L);
-		Motor_Stop(motor_R);
-	//}
+	int currentAngleL = Motor_GetEncoder(motor_L);
+	int currentAngleR = Motor_GetEncoder(motor_R);
+	int targetAngleL = currentAngleL+angle;
+	int targetAngleR = currentAngleR-angle;
+
+	Motor_Target(motor_L, targetAngleL);
+	Motor_Target(motor_R, targetAngleR);
+	Motor_Forward(motor_L, steerL);
+	Motor_Reverse(motor_R, steerR);
+
+	while ( (abs(Motor_GetEncoder(motor_L)-targetAngleL)>g_MoveTolerance) ||
+			(abs(Motor_GetEncoder(motor_R)-targetAngleR)>g_MoveTolerance) )
+				{;}
+	Motor_Stop(motor_L);
+	Motor_Stop(motor_R);
 }
 
-void Turn_Left(int time, int steerL, int steerR)
+void Turn_Left(int angle, int steerL, int steerR)
 {
-	//if ( (time>=g_RampDownLength) && (steerL>=10) )
-	//{
-	//	Motor_Reverse(motor_L, steerL);
-	//	Motor_Forward(motor_R, steerR);
-	//	Time_Wait(time - g_RampDownLength);
-	//	Motor_Reverse(motor_L, 10);
-	//	Motor_Forward(motor_R, 10);
-	//	Time_Wait(2);
-	//	Motor_Reverse(motor_L, 5);
-	//	Motor_Forward(motor_R, 5);
-	//	Time_Wait(4);
-	//	Motor_Reverse(motor_L, 3);
-	//	Motor_Forward(motor_R, 3);
-	//	Time_Wait(8);
-	//	Motor_Reverse(motor_L, 2);
-	//	Motor_Forward(motor_R, 2);
-	//	Time_Wait(4);
-	//	Motor_Reverse(motor_L, 1);
-	//	Motor_Forward(motor_R, 1);
-	//	Time_Wait(2);
-	//	Motor_Stop(motor_L);
-	//	Motor_Stop(motor_R);
-	//}
-	//else
-	//{
-		Motor_Reverse(motor_L, steerL);
-		Motor_Forward(motor_R, steerR);
-		Time_Wait(time);
-		Motor_Stop(motor_L);
-		Motor_Stop(motor_R);
-	//}
+	int currentAngleL = Motor_GetEncoder(motor_L);
+	int currentAngleR = Motor_GetEncoder(motor_R);
+	int targetAngleL = currentAngleL-angle;
+	int targetAngleR = currentAngleR+angle;
+
+	Motor_Target(motor_L, targetAngleL);
+	Motor_Target(motor_R, targetAngleR);
+	Motor_Reverse(motor_L, steerL);
+	Motor_Forward(motor_R, steerR);
+
+	while ( (abs(Motor_GetEncoder(motor_L)-targetAngleL)>g_MoveTolerance) ||
+			(abs(Motor_GetEncoder(motor_R)-targetAngleR)>g_MoveTolerance) )
+				{;}
+	Motor_Stop(motor_L);
+	Motor_Stop(motor_R);
 }
 
 void Lift_Up(int time, int power)
@@ -161,3 +97,163 @@ void Lift_Down(int time, int power)
 
 
 #endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//void Move_Forward(int time, int power=g_FullDrivePower)
+//{
+//	//if ( (time>=g_RampDownLength) && (power>=10) )
+//	//{
+//	//	Motor_Forward(motor_L, power);
+//	//	Motor_Forward(motor_R, power);
+//	//	Time_Wait(time - g_RampDownLength);
+//	//	Motor_Forward(motor_L, 10);
+//	//	Motor_Forward(motor_R, 10);
+//	//	Time_Wait(2);
+//	//	Motor_Forward(motor_L, 5);
+//	//	Motor_Forward(motor_R, 5);
+//	//	Time_Wait(4);
+//	//	Motor_Forward(motor_L, 3);
+//	//	Motor_Forward(motor_R, 3);
+//	//	Time_Wait(8);
+//	//	Motor_Forward(motor_L, 2);
+//	//	Motor_Forward(motor_R, 2);
+//	//	Time_Wait(4);
+//	//	Motor_Forward(motor_L, 1);
+//	//	Motor_Forward(motor_R, 1);
+//	//	Time_Wait(2);
+//	//	Motor_Stop(motor_L);
+//	//	Motor_Stop(motor_R);
+//	//}
+//	//else
+//	//{
+//		Motor_Forward(motor_L, power);
+//		Motor_Forward(motor_R, power);
+//		Time_Wait(time);
+//		Motor_Stop(motor_L);
+//		Motor_Stop(motor_R);
+//	//}
+//}
+
+
+//void Move_Backward(int time, int power=g_FullDrivePower)
+//{
+//	//if ( (time>=g_RampDownLength) && (power>=10) )
+//	//{
+//	//	Motor_Reverse(motor_L, power);
+//	//	Motor_Reverse(motor_R, power);
+//	//	Time_Wait(time - g_RampDownLength);
+//	//	Motor_Reverse(motor_L, 10);
+//	//	Motor_Reverse(motor_R, 10);
+//	//	Time_Wait(2);
+//	//	Motor_Reverse(motor_L, 5);
+//	//	Motor_Reverse(motor_R, 5);
+//	//	Time_Wait(4);
+//	//	Motor_Reverse(motor_L, 3);
+//	//	Motor_Reverse(motor_R, 3);
+//	//	Time_Wait(8);
+//	//	Motor_Reverse(motor_L, 2);
+//	//	Motor_Reverse(motor_R, 2);
+//	//	Time_Wait(4);
+//	//	Motor_Reverse(motor_L, 1);
+//	//	Motor_Reverse(motor_R, 1);
+//	//	Time_Wait(2);
+//	//	Motor_Stop(motor_L);
+//	//	Motor_Stop(motor_R);
+//	//}
+//	//else
+//	//{
+//		Motor_Reverse(motor_L, power);
+//		Motor_Reverse(motor_R, power);
+//		Time_Wait(time);
+//		Motor_Stop(motor_L);
+//		Motor_Stop(motor_R);
+//	//}
+//}
+
+//void Turn_Right(int time, int steerL, int steerR)
+//{
+//	//if ( (time>=g_RampDownLength) && (steerL>=10) )
+//	//{
+//	//	Motor_Forward(motor_L, steerL);
+//	//	Motor_Reverse(motor_R, steerR);
+//	//	Time_Wait(time - g_RampDownLength);
+//	//	Motor_Forward(motor_L, 10);
+//	//	Motor_Reverse(motor_R, 10);
+//	//	Time_Wait(2);
+//	//	Motor_Forward(motor_L, 5);
+//	//	Motor_Reverse(motor_R, 5);
+//	//	Time_Wait(4);
+//	//	Motor_Forward(motor_L, 3);
+//	//	Motor_Reverse(motor_R, 3);
+//	//	Time_Wait(8);
+//	//	Motor_Forward(motor_L, 2);
+//	//	Motor_Reverse(motor_R, 2);
+//	//	Time_Wait(4);
+//	//	Motor_Forward(motor_L, 1);
+//	//	Motor_Reverse(motor_R, 1);
+//	//	Time_Wait(2);
+//	//	Motor_Stop(motor_L);
+//	//	Motor_Stop(motor_R);
+//	//}
+//	//else
+//	//{
+//		Motor_Forward(motor_L, steerL);
+//		Motor_Reverse(motor_R, steerR);
+//		Time_Wait(time);
+//		Motor_Stop(motor_L);
+//		Motor_Stop(motor_R);
+//	//}
+//}
+
+//void Turn_Left(int time, int steerL, int steerR)
+//{
+//	//if ( (time>=g_RampDownLength) && (steerL>=10) )
+//	//{
+//	//	Motor_Reverse(motor_L, steerL);
+//	//	Motor_Forward(motor_R, steerR);
+//	//	Time_Wait(time - g_RampDownLength);
+//	//	Motor_Reverse(motor_L, 10);
+//	//	Motor_Forward(motor_R, 10);
+//	//	Time_Wait(2);
+//	//	Motor_Reverse(motor_L, 5);
+//	//	Motor_Forward(motor_R, 5);
+//	//	Time_Wait(4);
+//	//	Motor_Reverse(motor_L, 3);
+//	//	Motor_Forward(motor_R, 3);
+//	//	Time_Wait(8);
+//	//	Motor_Reverse(motor_L, 2);
+//	//	Motor_Forward(motor_R, 2);
+//	//	Time_Wait(4);
+//	//	Motor_Reverse(motor_L, 1);
+//	//	Motor_Forward(motor_R, 1);
+//	//	Time_Wait(2);
+//	//	Motor_Stop(motor_L);
+//	//	Motor_Stop(motor_R);
+//	//}
+//	//else
+//	//{
+//		Motor_Reverse(motor_L, steerL);
+//		Motor_Forward(motor_R, steerR);
+//		Time_Wait(time);
+//		Motor_Stop(motor_L);
+//		Motor_Stop(motor_R);
+//	//}
+//}
